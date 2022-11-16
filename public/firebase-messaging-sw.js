7a21515bd5f90dff2c47e50aadf6d411202659a0
zDudaHang <maria.eduarda.melo.hang@gmail.com>
2022-11-16 20:04:12 -0300

    revertendo bug no sw do firebase

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -5,12 +5,12 @@ importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compa
 // your app's Firebase config object.
 // https://firebase.google.com/docs/web/setup#config-object
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: 'AIzaSyAP_Nr9rkSJ0g7hvQKbjQI9UH_Jy6kK_u0',
+  authDomain: 'matricula-ufsc.firebaseapp.com',
+  projectId: 'matricula-ufsc',
+  storageBucket: 'matricula-ufsc.appspot.com',
+  messagingSenderId: '544318734893',
+  appId: '1:544318734893:web:5f826baad5a07047f32ea4',
 }
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
